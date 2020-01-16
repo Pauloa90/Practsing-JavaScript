b47a50dea465cbbc2484f39dcc35577459950a97
Pauloa90 <paulo.a90@hotmail.com>
2020-01-16 22:38:38 +0000

    learning

---
+++ ExerciciosCuroEmVideo/script.js
@@ -0,0 +1,18 @@
+function loading(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date();
+    hora = data.getHours();
+   
+    msg.innerHTML = `Agora sao ${hora} horas` 
+    if (hora >= 0 && hora < 12) {
+        img.src="morning.png"
+        document.body.style.background = '#EEDDC0'
+    } else if (hora >=12 && hora < 18){
+        img.src="afternoon.png"
+        document.body.style.background =  '#BD8A6F'
+    } else {
+        img.src="rsz_evening.png"
+        document.body.style.background = '#5F524A'
+    }
+}
