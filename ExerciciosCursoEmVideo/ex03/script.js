c66ffe3f8b328dac5ccbc3bbfff5e5472d23878f
Pauloa90 <paulo.a90@hotmail.com>
2020-01-17 11:34:37 +0000

    learning

---
+++ ExerciciosCursoEmVideo/ex03/script.js
@@ -0,0 +1,10 @@
+var s = window.document.getElementById('start')
+var s1 = Number(s.value)
+var e = window.document.getElementById('end')
+var e1 = Number(s1.value)
+var p = window.document.getElementById('pace')
+var p1 = Number(p.value)
+
+console.log(s1)
+console.log(e1)
+console.log(p1)
