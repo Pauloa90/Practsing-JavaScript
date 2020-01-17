4fdc674c2cbdfe714cd6c834b09935b8e54dc092
Pauloa90 <paulo.a90@hotmail.com>
2020-01-17 11:48:09 +0000

    learning

--- ExerciciosCursoEmVideo/ex03/script.js
+++ ExerciciosCursoEmVideo/ex03/script.js
@@ -1,10 +1,12 @@
-var s = window.document.getElementById('start')
-var s1 = Number(s.value)
-var e = window.document.getElementById('end')
-var e1 = Number(s1.value)
-var p = window.document.getElementById('pace')
-var p1 = Number(p.value)
+function verificar(){
+    var s = window.document.getElementById('start')
+    var s1 = Number(s.value)
+    var e = window.document.getElementById('end')
+    var e1 = Number(e.value)
+    var p = window.document.getElementById('pace')
+    var p1 = Number(p.value)
 
-console.log(s1)
-console.log(e1)
-console.log(p1)
+    for ( var c = s1 ; c <= e1 ; c += p1){
+        console.log(c)
+    }
+}
